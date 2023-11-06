36256ba95faff0c9272d04818ee824209bddd09d
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-11-06 06:04:35 +0000

    [add]top_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+  
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  root to: 'user/homes#top'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
