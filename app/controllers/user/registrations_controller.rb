bd53b4bd9a5e11828f4f7c3df7a23e8aa2271d28
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-11-27 05:58:07 +0000

    新規登録後のページ設定

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -50,9 +50,9 @@ class User::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
